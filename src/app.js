c694afe01adb69e367a45fc844999cb1e87b4edc
Ubirajara Neves <ubirajara.neves@usp.br>
2022-12-22 07:37:38 -0300

    Basic logging implemented.

--- src/app.js
+++ src/app.js
@@ -1,15 +1,38 @@
 import express from 'express';
+import winston from 'winston';
 
+// Server
 const app = express();
 const PORT = 3000;
 const okMessage = `Server OK. Listening on port ${PORT}.`;
 
 app.use(express.json());
 
+// Logger
+const { combine, timestamp, label, printf } = winston.format;
+const logFormat = printf(({ level, message, label, timestamp }) => {
+    return `${timestamp} [${label}] ${level}: ${message}`;
+});
+const logger = winston.createLogger({
+    level: 'silly',
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'xpe-bookstore-api.log' }),
+    ],
+    format: combine(
+        label({ label: 'xpe-bookstore-api' }),
+        timestamp(),
+        logFormat,
+    ),
+});
+
+// Router
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.status(200).send(okMessage);
 });
 
-app.listen(PORT, () => console.log(okMessage));
+app.listen(PORT, () => {
+    logger.info(okMessage);
+});
